8d54b4b1aad6bd84d8596cc07e86fc34476ee75e
unknown <surajjamdade83086@gmail.com>
2024-09-20 12:30:47 +0530

    first commit

---
+++ js/mobile.nav.js
@@ -0,0 +1,21 @@
+const indicator = document.querySelector(".nav-indicator-wrapper");
+const items = document.querySelectorAll(".nav-item");
+
+function handleIndicator(el) {
+  items.forEach((item) => {
+    item.classList.remove("is-active");
+  });
+
+  indicator.style.width = `${el.offsetWidth}px`;
+  indicator.style.left = `${el.offsetLeft}px`;
+
+  el.classList.add("is-active");
+}
+
+items.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    handleIndicator(item);
+  });
+
+  item.classList.contains("is-active") && handleIndicator(item);
+});
